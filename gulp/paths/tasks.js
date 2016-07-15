79c05cf1eb6748eac5d14f6ec2108921a16d6de6
Ilya <cuprum19930@gmail.com>
2016-07-15 20:37:59 +0500

    Removed browser-sync, made own static server

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -2,7 +2,7 @@
 
 module.exports = [
   './gulp/tasks/sass.js',
-  './gulp/tasks/serve.js',
+  // './gulp/tasks/serve.js',
   './gulp/tasks/jade.js',
   './gulp/tasks/watch.js',
   './gulp/tasks/clean.js',
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/skillsDB');
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json({type: 'application/json'}));
+
+app.use(express.static('build'));
+
+
+app.get('/*', function (req, res) {
+    res.setHeader('Content-Type', 'text/html; encoding: utf-8;');
+    console.log('Got new request at', req.url);
+    var fileName = './build/'+ req.url;
+    var content = fs.readFileSync(fileName, {encoding: 'utf-8'});
+    console.log('Server running');
+    res.write(content);
+    res.end();
+});
+
+
+app.listen(3035);
