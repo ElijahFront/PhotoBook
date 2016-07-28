111ec2d53fa2193f7ba890e7d88f4355a72ac9d0
Ilya <cuprum19930@gmail.com>
2016-07-28 10:24:10 +0500

    alb page rendering (с проверкой, есть ли фотографии в альбоме)

--- routes/albums.js
+++ routes/albums.js
@@ -5,19 +5,29 @@ module.exports = function (req, res){
       var albumID = req.params.album;
 
     Album.findOne({_id:albumID}, function (err, album) {
+        if (err) return next(err);
         var albumName = album.name,
             albumInfo = album.description;
 
         Photo.find({album: {$in: albumID}}, function (err, photo) {
             if (err) return next(err);
 
-            var photos = photo;
+            if (photo){
+                var photos = photo;
 
-            res.render('album', {
-                name:albumName,
-                info:albumInfo,
-                photos:photos
-            })
+                res.render('album', {
+                    name:albumName,
+                    info:albumInfo,
+                    photos:photos
+                })
+            } else {
+                res.render('album', {
+                    name:albumName,
+                    info:albumInfo
+                })
+            }
+
+
         })
     })
 };
